8d48e5a4c728e4661fcb08faae37c390c9c4fb71
Md. Nowshad Hasan <nowshadapu@gmail.com>
2020-02-03 18:32:40 +0600

    Add files for SMS Retriever API

--- app/src/main/java/com/example/smsverificationapiexample/MainActivity.java
+++ app/src/main/java/com/example/smsverificationapiexample/MainActivity.java
@@ -1,14 +1,117 @@
 package com.example.smsverificationapiexample;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private int RESOLVE_HINT = 1001;
+    private GoogleApiClient apiClient;
+    private String SMS_BODY_KEY = "";
+    private String SMS_BODY_VALUE = "Your ExampleApp code is: 123ABC78 \n 4H58dxQOfEu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSMSRetrieverAPI = findViewById(R.id.btn_SMSRetrieverAPI);
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
+        btnSMSRetrieverAPI.setOnClickListener(v -> {
+            SmsRetrieverClient client = SmsRetriever.getClient(this);
+
+            Task<Void> task = client.startSmsRetriever();
+//            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+//            sendIntent.putExtra("sms_body", "default content");
+//            sendIntent.setType("vnd.android-dir/mms-sms");
+//            startActivity(sendIntent);
+            task.addOnSuccessListener(aVoid -> {
+
+            });
+
+            task.addOnFailureListener(e -> {
+            });
+//            requestHint();
+//            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+//            sendIntent.putExtra("sms_body", "default content");
+//            sendIntent.setType("vnd.android-dir/mms-sms");
+//            startActivity(sendIntent);
+        });
+
+
+        AppSignatureHelper appSignatureHelper = new AppSignatureHelper(this);
+        for (String signature : appSignatureHelper.getAppSignatures()) {
+            Log.d("Main Activity", signature);
+        }
+
+    }
+
+    // Construct a request for phone numbers and show the picker
+    private void requestHint() {
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+        PendingIntent intent = Auth.CredentialsApi.getHintPickerIntent(
+                apiClient, hintRequest);
+        try {
+            startIntentSenderForResult(intent.getIntentSender(),
+                    RESOLVE_HINT, null, 0, 0, 0);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESOLVE_HINT) {
+            if (resultCode == RESULT_OK) {
+                Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+                if (credential != null) {
+                    Toast.makeText(this, credential.getId(), Toast.LENGTH_LONG).show();
+                }
+                // credential.getId();  <-- will need to process phone number string
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
+
+
